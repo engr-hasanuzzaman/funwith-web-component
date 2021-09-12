fe353104dfa4fcfe774b2a9ce4a8a36e5dafd951
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-12 19:48:59 +0600

    Create form web component and trigger custom event

--- custom_components/TodoApp/index.js
+++ custom_components/TodoApp/index.js
@@ -13,7 +13,7 @@ todoTemplate.innerHTML = `
         }
     </style>
     <div class='container'></div>
-    <button>Add New</button>
+    
 `;
 class TodoWidget extends HTMLElement {
     constructor(props) {
@@ -32,10 +32,40 @@ class TodoWidget extends HTMLElement {
         };
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(todoTemplate.content.cloneNode(true));
-        this.shadowRoot.querySelector('.container').innerHTML = this.state.todos.map(todo => `<todo-item-widget title="${todo.title}" checked=${todo.status}></todo-item-widget>`).join('');
+        const formContent = `
+            <todo-item-form></todo-item-form>
+        `
+        this.shadowRoot.querySelector('.container').innerHTML = this.state.todos.map(todo => `<todo-item-widget title="${todo.title}" checked=${todo.status}></todo-item-widget>`).join('').concat(formContent);
+        // bind custom event
+        this.shadowRoot.querySelector('.container').addEventListener('onNew', (e) => {
+            console.log('--------- on the parent: the value is ' + e.detail);
+        })
     }
 }
 
+// todo form component
+const formTemplate = document.createElement('template');
+formTemplate.innerHTML = `
+    <div>
+        <input type='text' placeholder='todo title'>
+        <button class='new-todo'>Add New</button>
+    </div>
+`
+class TodoItemForm extends HTMLElement {
+    constructor(props) {
+        super(props);
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(formTemplate.content.cloneNode(true));
+        this.shadowRoot.querySelector('.new-todo').addEventListener('click', () => {
+            const text = this.shadowRoot.querySelector('input').value;
+            console.log('---------the value is ' + text);
+            this.dispatchEvent(new Event('onNew', { detail: text }));
+        });
+    }
+    
+}
+
+// totoItem component
 class TodoItemWidget extends HTMLElement {
     constructor(props) {
         super(props);
@@ -52,5 +82,6 @@ class TodoItemWidget extends HTMLElement {
 
 };
 
+window.customElements.define('todo-item-form', TodoItemForm);
 window.customElements.define('todo-item-widget', TodoItemWidget);
 window.customElements.define('todo-widget', TodoWidget);
