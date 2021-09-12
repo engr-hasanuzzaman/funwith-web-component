0a546d17343e2145196d127567ccb014df7eb0ed
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-12 17:29:17 +0600

    Create todo-item-widget

--- custom_components/TodoApp/index.js
+++ custom_components/TodoApp/index.js
@@ -1,8 +1,27 @@
 class TodoWidget extends HTMLElement {
     constructor(props) {
         super(props);
-        this.innerHTML ="Todo web component";
+        this.innerHTML = `
+            <todo-item-widget title="1st todo"></todo-item-widget>
+            <todo-item-widget title="2nd todo"></todo-item-widget>
+        `;
     }
 }
 
+// const TodoElementTemplate = 
+class TodoItemWidget extends HTMLElement {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            title: this.getAttribute('title'),
+            status: this.getAttribute('status'),
+        }
+        this.innerHTML = `
+            <p>${this.state.title}</p>
+        `;
+    }
+    
+};
+
+window.customElements.define('todo-item-widget', TodoItemWidget);
 window.customElements.define('todo-widget', TodoWidget);
