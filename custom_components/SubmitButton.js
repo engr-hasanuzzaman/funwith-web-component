1a7b71c8173c189747bfad3f3f9123e1610d09da
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-24 23:57:04 +0600

    Style submit button

--- custom_components/SubmitButton.js
+++ custom_components/SubmitButton.js
@@ -7,7 +7,23 @@ class SubmitButton extends HTMLElement {
     connectedCallback() {
         this.label = this.getAttribute('label');
         this.innerHTML = `
-            <button type="submit">${this.label}</button>
+            <style>
+                .btn {
+                    height: 44px;
+                    padding: 0 16px;
+                    border: 1px solid #ced4da;
+                    border-radius: 6px;
+                    color: #fff;
+                    background-color: #007bff;
+                    border-color: #007bff;
+                    cursor: pointer;
+                }
+                .btn:hover {
+                    background-color: #0069d9;
+                    border-color: #0062cc;
+                }
+            </style>
+            <button type="submit" class="btn">${this.label}</button>
         `;
     }
 }
