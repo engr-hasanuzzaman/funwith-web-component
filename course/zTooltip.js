322ef293f0c0dde7130835832839b7e99ecfd173
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-26 16:45:12 +0600

    Tooltip with template

--- course/zTooltip.js
+++ course/zTooltip.js
@@ -1,27 +1,34 @@
+const template = `
+    <style>
+        div {
+            position: absolute;
+            background-color: black;
+            color: #ffffff;
+        }
+    </style>
+    <slot></slot>
+    <span class="info">(?)</span>
+`;
+
 class ZToolTip extends HTMLElement {
     constructor() {
         super();
         this.infoElement = null;
         this._tooltipText = 'Info will be show in here';
         this.attachShadow({ mode: 'open'});
+        this.shadowRoot.innerHTML = template;
     }
 
     connectedCallback() {
-        const icon = document.createElement('span');
+        const icon = this.shadowRoot.querySelector('span');
+        this.style.position = 'relative';
         if(this.hasAttribute('text')) {
             this._tooltipText = this.getAttribute('text');
         }
         this.infoElement = document.createElement('div');
         this.infoElement.textContent = this._tooltipText;
-        this.infoElement.style.position = 'absolute';
-        this.infoElement.style.backgroundColor = 'black';
-        this.infoElement.style.color = 'white';
-
-        icon.textContent = '(?)';
         icon.addEventListener('mouseenter', this._showInfo.bind(this));
         icon.addEventListener('mouseleave', this._hideInfo.bind(this));
-        this.shadowRoot.appendChild(icon);
-        this.style.position = 'relative';
     }
 
     _showInfo() {
