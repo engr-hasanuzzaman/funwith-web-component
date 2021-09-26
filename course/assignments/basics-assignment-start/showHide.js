39e1700da2d433360a052bcb2cc1235fe353b05f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-26 20:32:34 +0600

    Update show toggle with dynamic attribute

--- course/assignments/basics-assignment-start/showHide.js
+++ course/assignments/basics-assignment-start/showHide.js
@@ -1,30 +1,42 @@
 const template = (data) => `
     <style>
         .info-box {
-            display: none;
+            display: ${data.display};
         }
     </style>
     <button>${data.btnLabel}</button>
-    <p class="info-box"><slot></slot></p>
+    <p class="info-box">
+        <slot></slot>
+    </p>
 `;
 
 class ShowHideInfo extends HTMLElement {
     constructor() {
         super();
-        this.show = false;
         this.attachShadow({mode: 'open'});
     }
 
     connectedCallback() {
-        const btnLabel = this.getAttribute('btnLabel') || 'Show';
-        this.shadowRoot.innerHTML = template({btnLabel});
+        this.show = this.hasAttribute('show');
+        let btnLabel;
+        let display;
+        if(this.show) {
+            btnLabel = 'Hide';
+            display = 'block';
+        } else {
+            btnLabel = 'Show'
+            display = 'none';
+        }
+        this.shadowRoot.innerHTML = template({ btnLabel, display });
         const btn = this.shadowRoot.querySelector('button');
         const info = this.shadowRoot.querySelector('.info-box');
         btn.addEventListener('click', event => {
             if(this.show) {
                 info.style.display = 'none';
+                btn.innerText = 'Show';
             } else {
                 info.style.display = 'block';
+                btn.innerText = 'Hide';
             }
 
             this.show = !this.show;
