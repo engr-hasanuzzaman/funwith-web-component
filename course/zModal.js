2bf7ffa24a320e157d343906600af27d0568b64e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-27 23:54:32 +0600

    Use static header and footer and apply basic style

--- course/zModal.js
+++ course/zModal.js
@@ -25,10 +25,38 @@ class ZModal extends HTMLElement {
                     box-shadow: 0 2px 3px rgba(0,0,0,0.20);
                     z-index: 11; 
                     min-height: 20vh;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    padding: 10px;
+                }
+
+                #footer {
+                    display: flex;
+                    justify-content: flex-end;
+                    margin-bottom: 10px;
+                }
+
+                #footer button {
+                    margin: 0 0.25rem;
+                    cursor: pointer;
                 }
             </style>
             <div id="back-drop"></div>
-            <div id="modal"></div>
+            <div id="modal">
+                <header>
+                    <h3>Please confirm</h3>
+                </header>
+
+                <section id="main-body">
+                    <slot></slot>
+                </section>
+
+                <section id="footer">
+                    <button>Confirm</button>
+                    <button>Cancel</button>
+                </section>
+            </div>
         `;
     }
 }
