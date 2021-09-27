cf5dca552833a3983abbfec3ef4809982cf5936a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-27 23:27:33 +0600

    Style the modal backdrop

--- course/zModal.js
+++ course/zModal.js
@@ -1,6 +1,21 @@
 class ZModal extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                #back-drop {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    background-color: rgba(0,0,0,0.3);
+                    width: 100%;
+                    height: 100vh;
+                }
+            </style>
+            <div id="back-drop"></div>
+            <div id="modal"></div>
+        `;
     }
 }
 
