7688ff4c5602d7529b0e1af530f7a42b3fc7e9b8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-09-27 23:34:23 +0600

    Basic  style of the modal

--- course/zModal.js
+++ course/zModal.js
@@ -11,6 +11,20 @@ class ZModal extends HTMLElement {
                     background-color: rgba(0,0,0,0.3);
                     width: 100%;
                     height: 100vh;
+                    z-index: 10px;
+                }
+
+                #modal {
+                    position: fixed;
+                    top: 20vh;
+                    left: 25%;
+                    width: 50%;
+                    background-color: #fff;
+                    border: 3px solid gray;
+                    border-radius: 6px;
+                    box-shadow: 0 2px 3px rgba(0,0,0,0.20);
+                    z-index: 11; 
+                    min-height: 20vh;
                 }
             </style>
             <div id="back-drop"></div>
