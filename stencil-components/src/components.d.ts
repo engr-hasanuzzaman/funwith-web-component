ea5fd3343366a543eab7831944fea31dec87d75f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-02 00:41:43 +0600

    Update side-drawer to pass title property

--- stencil-components/src/components.d.ts
+++ stencil-components/src/components.d.ts
@@ -21,6 +21,7 @@ export namespace Components {
         "middle": string;
     }
     interface SideDrawer {
+        "title": string;
     }
 }
 declare global {
@@ -57,6 +58,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface SideDrawer {
+        "title"?: string;
     }
     interface IntrinsicElements {
         "my-component": MyComponent;
--- stencil-components/src/components/side-drawer/side-drawer.tsx
+++ stencil-components/src/components/side-drawer/side-drawer.tsx
@@ -6,10 +6,18 @@ import { Component, h, Prop } from '@stencil/core';
     shadow: true
 })
 export class SideDrawer {
+    @Prop() title: string;
+
     render() {
         return(
             <aside>
+                <header>
+                    <h1>{this.title}</h1>
+                </header>
 
+                <main>
+                    <slot />
+                </main>
             </aside>
         );
     }
