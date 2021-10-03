138be618064ecdd7b8275479174328af3a59ccdc
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-03 11:21:16 +0600

    Fetch price from the api and show on the view

--- stencil-components/src/components/stock-price/stock-price.tsx
+++ stencil-components/src/components/stock-price/stock-price.tsx
@@ -1,19 +1,32 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, State } from "@stencil/core";
 
 @Component({
     tag: 'z-stock-price',
     shadow: true
 })
 export class StockPrice {
+    @State() price: string;
 
+    fetchPrice(e: Event) {
+        e.preventDefault();
+
+        fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=YV1ATWSX9DJBMBD1')
+        .then(resp => {
+            return resp.json();
+        })
+        .then(parsedResp =>  {
+            console.log("the price is ", parsedResp["Global Quote"]["05. price"]);
+            this.price = parsedResp["Global Quote"]["05. price"];
+        })
+    }
     render() {
         return([
-            <div>
+            <form onSubmit={this.fetchPrice.bind(this)}>
                 <input type="text" name="" id="price" />
                 <button>Fetch</button>
-            </div>,
+            </form>,
             <div>
-                <p>Price:</p>
+                <p>Price: {this.price}</p>
             </div>
         ]);
     }
