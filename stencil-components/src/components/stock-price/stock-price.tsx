30f4c4d8f82a9ccadf0014f6fca54344ba35921d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-03 09:34:53 +0600

    Start creating new component with advance stencial features

---
+++ stencil-components/src/components/stock-price/stock-price.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from "@stencil/core";
+
+@Component
+export class StockPrice {
+
+    render() {
+        return(
+            <div>
+                <h1>Stock Price</h1>
+            </div>
+        );
+    }
+}
