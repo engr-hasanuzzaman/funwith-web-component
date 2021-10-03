7241791ed98c7cebf69ede3de9324c6e7220462c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-10-03 11:43:14 +0600

    Take user input to fetch the price

--- stencil-components/src/components/stock-price/stock-price.tsx
+++ stencil-components/src/components/stock-price/stock-price.tsx
@@ -1,15 +1,16 @@
-import { Component, h, State } from "@stencil/core";
+import { Component, h, State, Element } from "@stencil/core";
 import { AV_API_KEY } from "../../global/global";
 @Component({
     tag: 'z-stock-price',
     shadow: true
 })
 export class StockPrice {
+    @Element() el: HTMLElement;
     @State() price: number;
 
     fetchPrice(e: Event) {
-        const symbol = 'IBM';
         e.preventDefault();
+        const symbol = (this.el.shadowRoot.querySelector('#symbol') as HTMLInputElement).value;
 
         fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${AV_API_KEY}`)
         .then(resp => {
